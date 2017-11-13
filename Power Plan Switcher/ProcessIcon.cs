fe981f0021c1a980f82bfb10ae287be1f0d8133d
Daniel Mar <mardaniel42@gmail.com>
2017-11-13 13:07:10 +1300

    First Commit

---
+++ Power Plan Switcher/ProcessIcon.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Taskbar_Test.Properties;
+
+namespace Taskbar_Test
+{
+    class ProcessIcon : IDisposable
+    {
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerSetActiveScheme(IntPtr UserRootPowerKey,
+                    ref Guid SchemeGuid);
+
+        NotifyIcon ni;
+
+        private ContextMenuStrip menu;
+        private ToolStripMenuItem item;
+        private ToolStripLabel label;
+        private ToolStripSeparator sep;
+
+        private List<ToolStripMenuItem> itemList = new List<ToolStripMenuItem>();
+
+        private List<Guid> GUIDs;
+        private List<String> names;
+        private Guid active;
+
+        public ProcessIcon(List<Guid> guidsIn, List<String> namesIn, Guid activeIn)
+        {
+            GUIDs = guidsIn;
+            names = namesIn;
+            active = activeIn;
+
+            ni = new NotifyIcon();
+        }
+
+        public void Display()
+        {
+            ni.Icon = Resources.icon;
+            ni.Text = "Power Plan Switcher v.0.1";
+
+            menu = new ContextMenuStrip();
+            label = new ToolStripLabel();
+            sep = new ToolStripSeparator();
+
+            menu.Name = "Menu";
+            menu.Size = new Size(153, 70);
+
+            label.Text = "Power Plan Switcher v.0.1";
+            label.Font = new Font(label.Font, FontStyle.Bold);
+            menu.Items.Add(label);
+
+            menu.Items.Add(sep);
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                item = new ToolStripMenuItem();
+                item.Text = names[i];
+                Guid guid = GUIDs[i];
+                if (guid == active) item.Checked = true;
+                item.Click += (sender, e) => Change_Plan(sender, e, guid);
+                itemList.Add(item);
+                menu.Items.Add(item);
+            }
+
+            sep = new ToolStripSeparator();
+            menu.Items.Add(sep);
+
+            item = new ToolStripMenuItem();
+            item.Text = "Close";
+            item.Click += Exit_Click;
+            menu.Items.Add(item);
+
+            ni.ContextMenuStrip = menu;
+            ni.MouseClick += Icon_Click;
+            ni.Visible = true;
+        }
+
+        private void Icon_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu",
+                         BindingFlags.Instance | BindingFlags.NonPublic);
+                mi.Invoke(ni, null);
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Dispose();
+            Application.Exit();
+        }
+
+        private void Change_Plan(object sender, EventArgs e, Guid target)
+        {
+            uint resp = PowerSetActiveScheme(IntPtr.Zero, ref target);
+
+            if (resp != 0) throw new Exception("Error in changing the power plan.");
+
+            itemList.ForEach(delegate (ToolStripMenuItem check)
+            {
+                if (check == sender) check.Checked = true;
+                else if (check != null) check.Checked = false;
+            });
+        }
+
+        public void Dispose()
+        {
+            ni.Dispose();
+        }
+    }
+}
---
+++ Power Plan Switcher/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taskbar_Test
+{
+    static class Program
+    {
+        enum PowerDataAccessor : uint
+        {
+            ACCESS_SCHEME = 16
+        }
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerEnumerate(IntPtr RootPowerKey,
+                    IntPtr SchemeGuid,
+                    IntPtr SubGroupOfPowerSettingsGuid,
+                    PowerDataAccessor AccessFlags,
+                    uint Index,
+                    IntPtr Buffer,
+                    ref uint BufferSize);
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerReadFriendlyName(IntPtr RootPowerKey,
+                    ref Guid SchemeGuid,
+                    IntPtr SubGroupOfPowerSettingGuid,
+                    IntPtr PowerSettingGuid,
+                    IntPtr Buffer,
+                    ref UInt32 BufferSize);
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerGetActiveScheme(IntPtr UserRootPowerKey,
+                    ref IntPtr ActivePolicyGuid);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            List<Guid> GUIDs = new List<Guid>();
+            List<String> names = new List<String>();
+
+            uint returnCode = 0;
+
+            uint index = 0;
+            uint buffersize = 16;
+            IntPtr readbuffer;
+
+            while (returnCode == 0)
+            {
+                readbuffer = Marshal.AllocHGlobal((int)buffersize);
+
+                returnCode = PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, PowerDataAccessor.ACCESS_SCHEME, index, readbuffer, ref buffersize);
+                if (returnCode == 259) break;
+
+                GUIDs.Add((Guid)Marshal.PtrToStructure(readbuffer, typeof(Guid)));
+
+                uint reqSize = 0;
+                Guid planguid = GUIDs[(int)index];
+                String name = "Unnamed";
+
+                returnCode = PowerReadFriendlyName(IntPtr.Zero, ref planguid, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, ref reqSize);
+                if (returnCode == 0)
+                {
+                    //Request was successful
+                    if (reqSize == 0u) name = "";
+                    else
+                    {
+                        IntPtr ptrName = Marshal.AllocHGlobal((int)reqSize);
+                        returnCode = PowerReadFriendlyName(IntPtr.Zero, ref planguid, IntPtr.Zero, IntPtr.Zero, ptrName, ref reqSize);
+                        if (returnCode == 0) name = Marshal.PtrToStringUni(ptrName);
+                        Marshal.FreeHGlobal(ptrName);
+                    }
+                    names.Add(name);
+                }
+
+                Marshal.FreeHGlobal(readbuffer);
+
+                Console.WriteLine(GUIDs[(int)index]);
+                Console.WriteLine(names[(int)index]);
+
+                index++;
+            }
+
+            IntPtr activeGuid = IntPtr.Zero;
+            PowerGetActiveScheme(IntPtr.Zero, ref activeGuid);
+
+            Guid active = (Guid)Marshal.PtrToStructure(activeGuid, typeof(Guid));
+
+            using (ProcessIcon pi = new ProcessIcon(GUIDs, names, active))
+            {
+                pi.Display();
+
+                Application.Run();
+            }
+        }
+    }
+}
